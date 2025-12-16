85f472ee3990601ccf45ec9b1436f9481bf7e537
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-16 16:37:50 +0300

    Proje dosyası ekle.

---
+++ models.js
@@ -0,0 +1,15 @@
+class Student {
+    export default class Student { 
+        constructor(id, name, courses) {
+            Object.defineProperty(this, "id", {
+                value: id,
+                writable: false,
+                configurable: false
+            });
+            this.name = name;
+            this.courses = courses;
+        }
+    }
+
+
+}
