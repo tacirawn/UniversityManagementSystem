6f8ef3caecdea32570da4e95c92cb4c21960bcb6
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-16 20:41:22 +0300

    commit

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import Student from './models.js';
-import fetchStudents from './database.js';
-import calculateClassAverage, { filterStudents } from './analytics.js';
-import findTopStudent from './analytics.js';
-import filterStudents from './analytics.js';
+import { fetchStudents } from './database.js';
+import {calculateClassAverage, findTopStudent, filterStudents} from './analytics.js';
 
 
 console.log("Fetching data from database...");
@@ -14,13 +12,17 @@ fetchStudents((rawData) => {
     console.log("Original ID: ", students[0].id);
     console.log("Attempting to change the ID to 999...");
 
-    students[0].id = 999;
+    try{
+        students[0].id = 999;
+    }catch(error){
+        console.log("ID didn't change.")
+    }
 
     console.log("Final ID: ", students[0].id);
 
     console.log("--- Analytics Report ---");
     console.log("Class Average for Course 101: ", calculateClassAverage(students, 101));
-    console.log("Top Student: ", findTopStudent(students).name, "(Average: ", findTopStudent(students).getAverage, ")");
+    console.log("Top Student: ", findTopStudent(students).name, "(Average: ", findTopStudent(students).getAverage(), ")");
     console.log("Students in Course 102: ", filterStudents(students, (student) => student.courses.some(course => course.courseId === 102)));
 });
 
