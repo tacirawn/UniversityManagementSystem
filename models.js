778c175c941dcf6509470f3331622048bf5b4f3d
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-16 16:45:43 +0300

    commit

--- models.js
+++ models.js
@@ -1,15 +1,19 @@
-class Student {
-    export default class Student { 
-        constructor(id, name, courses) {
-            Object.defineProperty(this, "id", {
-                value: id,
-                writable: false,
-                configurable: false
-            });
-            this.name = name;
-            this.courses = courses;
-        }
+export default class Student {
+    constructor(id, name, courses) {
+        Object.defineProperty(this, "id", {
+            value: id,
+            writable: false,
+            configurable: false
+        });
+        this.name = name;
+        this.courses = courses;
     }
 
+    addCourse(courseId, grade) {
+        
+    }
 
+    getAverage() {
+
+    }
 }
