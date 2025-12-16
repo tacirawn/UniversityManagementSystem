22829013d224f10fb772823cfcf29a560c54a0f7
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-16 17:04:24 +0300

    commit

---
+++ database.js
@@ -0,0 +1,11 @@
+function fetchStudents(callback) {
+    setTimeout(() => {
+        const studentArray[Student] = [
+            { id: 1, name: "Ali", courses: [{ courseId: 101, grade: 90 }, { courseId: 102, grade: 85 }] },
+            { id: 2, name: "Zeynep", courses: [{ courseId: 101, grade: 70 }, { courseId: 102, grade: 95 }] },
+            { id: 3, name: "Ahmet", courses: [{ courseId: 101, grade: 60 }, { courseId: 102, grade: 55 }] }
+        ];
+    }, 2000);
+
+    callback(studentarray);
+}
